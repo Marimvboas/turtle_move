a81eeb7276489dfbaf9789e811b17706e54b62de
Marimvboas <marivboas02@gmail.com>
2020-10-15 14:12:10 -0300

    Add new script

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        	'moving = turtle_move.move:main',
+        	'moving = turtle_move.move2:main',
         ],
     },
 )
